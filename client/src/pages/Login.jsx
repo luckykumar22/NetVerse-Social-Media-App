5cd248335b2d27448cb329948d30ab4081192f36
luck22y <luckykumar2204@gmail.com>
2023-08-30 14:13:05 +0530

    Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,153 @@
+import { TbSocial } from "react-icons/tb";
+import { BsShare } from "react-icons/bs";
+import { AiOutlineInteraction } from "react-icons/ai";
+import { ImConnection } from "react-icons/im";
+
+import { CustomButton, Loading, TextInput } from "../components";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { BgImage } from "../assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [errMsg, setErrMsg] = useState();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {};
+
+  return (
+    <div className="bg-bgColor w-full h-[100vh] p-6 flex items-center justify-center">
+      <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary overflow-hidden shadow-xl rounded-xl">
+        {/* Left */}
+        <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
+          <div className="w-full flex gap-2 items-center mb-6">
+            <div className="p-2 rounded text-white bg-[#065ad8]">
+              <TbSocial />
+            </div>
+            <span className="text-2xl text-[#065ad8] font-semibold">
+              NetVerse
+            </span>
+          </div>
+          <p className="text-ascent-1 text-base font-semibold">
+            Log in to your account
+          </p>
+          <span className="text-ascent-2 text-sm mt-2">Welcome back</span>
+
+          <form
+            onSubmit={handleSubmit(onsubmit)}
+            className="flex flex-col gap-2 py-8"
+          >
+            <TextInput
+              name="email"
+              placeholder="email@example.com"
+              label="Email Address"
+              type="email"
+              register={register("email", {
+                required: "Email Address is required",
+              })}
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              error={errors.email ? errors.email.message : ""}
+            />
+
+            <TextInput
+              name="password"
+              placeholder="Password"
+              label="Password"
+              type="password"
+              register={register("password", {
+                required: "Password is required",
+              })}
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              error={errors.password ? errors.password.message : ""}
+            />
+
+            <Link
+              to="/reset-password"
+              className="text-right font-semibold text-blue text-sm "
+            >
+              Forgot Password?
+            </Link>
+
+            {errMsg?.message && (
+              <span
+                className={`text-sm ${
+                  errMsg?.status == "failed"
+                    ? "text-[#f64949fe]"
+                    : "text-[#2ba150fe]"
+                } mt-0.5`}
+              >
+                {errMsg?.message}
+              </span>
+            )}
+
+            {isSubmitting ? (
+              <Loading />
+            ) : (
+              <CustomButton
+                type="button"
+                title="Login"
+                containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+              />
+            )}
+          </form>
+
+          <p className="text-ascent-2 text-sm text-center">
+            Don't Have an account?
+            <Link
+              to="/register"
+              className="text-[#065ad8] ml-2 cursor-pointer font-semibold"
+            >
+              Create an account
+            </Link>
+          </p>
+        </div>
+        {/* Right */}
+        <div className="hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-blue">
+          <div className="relative w-full flex items-center justify-center">
+            <img
+              src={BgImage}
+              alt="Bg Image"
+              className="w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover"
+            />
+
+            <div className="absolute flex items-center gap-1 bg-white right-10 top-10 py-2 px-5 rounded-full">
+              <BsShare size={14} />
+              <span className="text-xs font-medium">Share</span>
+            </div>
+
+            <div className="absolute flex items-center gap-1 bg-white left-10 py-2 px-5 top-6 rounded-full">
+              <ImConnection />
+              <span className="text-xs font-medium">Connect</span>
+            </div>
+
+            <div className="absolute flex items-center gap-1 bg-white left-12 py-2 px-5 bottom-6 rounded-full">
+              <AiOutlineInteraction />
+              <span className="text-xs font-medium">Interact</span>
+            </div>
+          </div>
+
+          <div className="mt-16 text-center">
+            <p className="text-white text-base">
+              Connect with friends & have share for fun
+            </p>
+            <span className="text-sm text-white/80">
+              Share memories with friends and the world.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
